4a3f9d6d4a0b2cb3416764a6556d19303dc09223
Murkrow <marcocoppola02.mc@gmail.com>
2022-10-11 16:38:58 +0200

    Auto stash before merge of "master" and "origin/master"

--- mnkgame/run.sh
+++ mnkgame/run.sh
@@ -2,5 +2,5 @@
 cd out/mnkgame
 clear && printf '\e[3J'
 #java -cp ".." mnkgame.MNKPlayerTester $1 $2 $3 mnkgame.IterativeDeep mnkgame.IterativeDeep
-java -cp ".." mnkgame.MNKGame $1 $2 $3 mnkgame.TestZob mnkgame.TestZob
+java -cp ".." mnkgame.MNKGame $1 $2 $3 mnkgame.ZobristPlayer mnkgame.ZobristPlayer
 #MiniMaxAlphaBeta
